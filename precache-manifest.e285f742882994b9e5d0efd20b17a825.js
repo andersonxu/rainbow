aa056bd217532b7d5e645aef8ee0cb28f50f4e3b
Anderson <andersonxu88@gmail.com>
2020-02-11 12:22:39 +1030

    ini

---
+++ precache-manifest.e285f742882994b9e5d0efd20b17a825.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b254040c14ca7f644595f66d369cb5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0de0f62cbf98c53e7ec7",
+    "url": "./static/js/2.81ff1761.chunk.js"
+  },
+  {
+    "revision": "2f915040f1240a60f4e49f29679e131c",
+    "url": "./static/js/2.81ff1761.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2993b829482482e6468c",
+    "url": "./static/js/main.0631e7cf.chunk.js"
+  },
+  {
+    "revision": "353824c915518b56d5bd",
+    "url": "./static/js/runtime-main.1130748e.js"
+  },
+  {
+    "revision": "af87f73e09057ab5b150c83b4f9fd79a",
+    "url": "./static/media/ab1.af87f73e.svg"
+  },
+  {
+    "revision": "64b9999169d4036eb2fc0afadead21c3",
+    "url": "./static/media/ab2.64b99991.svg"
+  },
+  {
+    "revision": "3f27e1d68be06cc5f47718498bc3fe58",
+    "url": "./static/media/ab3.3f27e1d6.svg"
+  },
+  {
+    "revision": "fffdfa6cbe6fd6c5ea8a544bcecb6c2c",
+    "url": "./static/media/home.fffdfa6c.svg"
+  },
+  {
+    "revision": "1fc8cc7e52b7aa0cdaf7199da431a5d5",
+    "url": "./static/media/mapLand.1fc8cc7e.svg"
+  },
+  {
+    "revision": "3bfa4a91c78b3c903e7265219c5fd3a8",
+    "url": "./static/media/mapRoad.3bfa4a91.svg"
+  },
+  {
+    "revision": "9e848a104ae120f170d595bd06cd1c11",
+    "url": "./static/media/sitting-4.9e848a10.svg"
+  },
+  {
+    "revision": "95ef9c07096088ca9a6ef4e4e8083102",
+    "url": "./static/media/sitting-7.95ef9c07.svg"
+  },
+  {
+    "revision": "d81b55d3f426cbc26063701ab7b5f112",
+    "url": "./static/media/standing-18.d81b55d3.svg"
+  },
+  {
+    "revision": "a726a18bd0885c1da4489988fa44d227",
+    "url": "./static/media/standing-19.a726a18b.svg"
+  },
+  {
+    "revision": "c7a5f6f9957b09719cf9a7b938cc3141",
+    "url": "./static/media/standing-2.c7a5f6f9.svg"
+  }
+]);
